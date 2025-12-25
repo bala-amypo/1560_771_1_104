4e6b19e5410477de317d91417b907e701b38ed68
bala-amypo <bala@amypo.in>
2025-12-25 11:23:48 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceCatalogItemRepository.java
+++ src/main/java/com/example/demo/repository/DeviceCatalogItemRepository.java
@@ -1,14 +1,5 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.DeviceCatalogItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceCatalogItemRepository extends JpaRepository<DeviceCatalogItem, Long> {
-    DeviceCatalogItem findByDeviceCode(String deviceCode);
-    boolean existsByDeviceCode(String deviceCode);
-}
-package com.example.demo.repository;
-
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
